d85d0b03b9b614c4e983ef7b9a9470f7ea646f6c
S. Poole <spoole@uk.ibm.com>
2016-09-09 12:19:27 +0100

    Replaced java api call to watson translator with external scripts

--- src/main/java/com/ibm/javaone2016/demo/furby/sensor/TextToSpeechSensor.java
+++ src/main/java/com/ibm/javaone2016/demo/furby/sensor/TextToSpeechSensor.java
@@ -1,29 +1,20 @@
 package com.ibm.javaone2016.demo.furby.sensor;
 
-import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.Proxy;
+import java.net.URLEncoder;
 
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteWatchdog;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.ibm.javaone2016.demo.furby.AbstractActiveSensor;
-import com.ibm.watson.developer_cloud.service.WatsonService;
 import com.ibm.watson.developer_cloud.text_to_speech.v1.TextToSpeech;
-import com.ibm.watson.developer_cloud.text_to_speech.v1.util.WaveUtils;
-import com.squareup.okhttp.Authenticator;
-import com.squareup.okhttp.Credentials;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
 
 /*
  * Output sensor for converting a command into speech 
@@ -72,61 +63,35 @@ public class TextToSpeechSensor extends AbstractActiveSensor {
 		if (e == null)
 			return;
 		String text = e.getAsString();
-		byte[] sound = getAudioTranslation(text);
-		ByteArrayInputStream bin = new ByteArrayInputStream(sound);
-		AudioInputStream audioInputStream = null;
-		InputStream wav = WaveUtils.reWriteWaveHeader(bin);
-		audioInputStream = AudioSystem.getAudioInputStream(wav);
-		AudioFormat audioFormat = audioInputStream.getFormat();
-		SourceDataLine line = null;
-		DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
-		line = (SourceDataLine) AudioSystem.getLine(info);
-		line.open(audioFormat);
-		line.start();
-
-		int nBytesRead = 0;
-		byte[] abData = new byte[250000];
-		while (nBytesRead != -1) {
-			try {
-				nBytesRead = audioInputStream.read(abData, 0, abData.length);
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
-			if (nBytesRead >= 0) {
-				int nBytesWritten = line.write(abData, 0, nBytesRead);
-			}
-		}
-
-		line.drain();
-		line.close();
-
+		File sound = getAudioTranslation(text);
+		CommandLine cmdLine = CommandLine.parse("aplay "+sound.getAbsolutePath());
+		DefaultExecutor executor = new DefaultExecutor();
+		executor.setExitValue(0);
+		ExecuteWatchdog watchdog = new ExecuteWatchdog(60000);
+		executor.setWatchdog(watchdog);
+		int exitValue = executor.execute(cmdLine);
+		
 	}
 
-	private byte[] getAudioTranslation(String text) throws IOException {
+	private File getAudioTranslation(String text) throws IOException {
 
-		OkHttpClient client = new OkHttpClient();
-		client.setAuthenticator(new Authenticator() {
-
-			@Override
-			public Request authenticate(Proxy proxy, Response response) throws IOException {
-				String credential = Credentials.basic(userid, password);
-				return response.request().newBuilder().header("Authorization", credential).build();
-			}
-
-			@Override
-			public Request authenticateProxy(Proxy proxy, Response response) throws IOException {
-				return null;
-			}
-		});
-		Request request = new Request.Builder()
-
-				.url("https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize?accept=audio/wav&text=" + text
-						+ "&voice=en-US_AllisonVoice")
-				.build();
-		Response response = client.newCall(request).execute();
-		byte[] sound = response.body().bytes();
-		LOG.info("got {} bytes", sound.length);
-		return sound;
+		long then=System.currentTimeMillis();
+		File result=new File("/tmp/say.wav");
+		
+		String command ="/usr/bin/curl -s  -X GET -u \""+userid+"\":\""+password+"\" --output "+ result.getAbsolutePath()+ " \"https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize?accept=audio/wav&text="+URLEncoder.encode(text)+"&voice=en-US_AllisonVoice\"";
+		
+		CommandLine cmdLine = CommandLine.parse(command);
+		DefaultExecutor executor = new DefaultExecutor();
+		executor.setExitValue(0);
+		ExecuteWatchdog watchdog = new ExecuteWatchdog(60000);
+		executor.setWatchdog(watchdog);
+		int exitValue = executor.execute(cmdLine);
+		long now=System.currentTimeMillis();
+		
+		
+		LOG.info("got {} bytes in {} millseconds", result.length(), now-then);
+		
+		return result;
 
 	}
 
